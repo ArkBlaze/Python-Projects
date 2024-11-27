4bd9c6ac9365641b187bee60b6a65b6138262cf5
@pooveannan <58944387+ArkBlaze@users.noreply.github.com>
2024-11-27 10:15:28 +0530

    Rename task.py to Day 1/Brand Name Generator.py

---
+++ Day 1/Brand Name Generator.py
@@ -0,0 +1,2 @@
+print("Hello- Good Morning:)")
+print(input("Born city name:") + " " + input("Your Pet name:"))
--- task.py
+++
@@ -1,2 +0,0 @@
-print("Hello- Good Morning:)")
-print(input("Born city name:") + " " + input("Your Pet name:"))
